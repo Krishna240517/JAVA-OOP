7a4d9be8985715569d813cb6ce1600230d79f55d
Krishna Prasad Swain <krishna.prasad.swain2405@gmail.com>
2024-12-31 15:55:33 +0000

    staticblock

--- OOP/StaticAndInstanceBlock/StaticBlockExample/Main.java
+++ OOP/StaticAndInstanceBlock/StaticBlockExample/Main.java
@@ -1,5 +1,5 @@
 package OOP.StaticAndInstanceBlock.StaticBlockExample;
-
+//static block example
 public class Main{
     public static void main(String[] args) {
         Taxation t = new Taxation(50000);
